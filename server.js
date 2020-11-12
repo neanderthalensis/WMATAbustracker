13a3ed6c496b788a64864492ce9b129a703feb85
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-11 23:18:59 -0500

    pretty please

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http'); 
+const request = require('request');
+
+	console.log("hola")
 
 var port = process.env.PORT || 5000; 
 http.createServer(function(req,res){ // creates a server
     res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
-    res.end("hello"); //shows the text "hello" on th eweb page
+    res.end("hello"); 
     console.log("hihihih")
 }).listen(port); // attaches this server to the port no.
