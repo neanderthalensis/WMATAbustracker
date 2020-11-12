6ee5b0d4df1ddfb3b57b98e33fe1d007b15feb95
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-11 21:58:33 -0500

    pretty please

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const http = require('http'); //loads the library to enable it to act as a server
-var port = process.env.PORT || 5000; //specifies the port no to whatever heroku gives or 5000 on local host
+const http = require('http'); 
+
+var port = process.env.PORT || 5000; 
 http.createServer(function(req,res){ // creates a server
     res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
     res.end("hello"); //shows the text "hello" on th eweb page
+    console.log("hihihih")
 }).listen(port); // attaches this server to the port no.
