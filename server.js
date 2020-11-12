24375cee978e31e09e2e7caa253e6baccff140e7
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-12 13:08:55 -0500

    pretty please

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const http = require('http');
 const request = require('request');
 const { Client } = require('pg');
 
-const client = new Client({ //connects to the database
-  connectionString: process.env.DATABASE_URL,
-  ssl: {rejectUnauthorized: false}
-});
-client.connect();
-
-client.query("CREATE TABLE bus(VehicleID int, DirectionNum int, Lat decimal, Lon decimal)")
 function getBus(bus, key){
 	var imp1 = 'https://api.wmata.com/Bus.svc/json/jBusPositions?RouteID='+bus+'&api_key='+key
 	return new Promise(function(resolve, reject){
@@ -19,11 +12,28 @@ function getBus(bus, key){
 })});
 }
 
+
+const client = new Client({ //connects to the database
+  connectionString: process.env.DATABASE_URL,
+  ssl: {rejectUnauthorized: false}
+});
+client.connect();
+client.query("DROP TABLE bus;")
+client.query("CREATE TABLE bus(ts TIMESTAMP PRIMARY KEY, json JSON)")
+
+
+/*
 (async () => {
 	var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79');
 	console.log(bla);
 })()
-
+*/
+(async () => {
+	var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79');
+	var qimp =  await "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
+	var qvals = await [new Date(), bla]
+	client.query(qimp, bla)
+})()
 /*
 var port = process.env.PORT || 5000; 
 http.createServer(function(req,res){ // creates a server
