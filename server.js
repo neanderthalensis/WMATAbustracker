7bdbcda6536a0a7da2a13ce4ca2e1bd26ea99f57
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-12 10:59:47 -0500

    pretty please

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require('http'); 
 const request = require('request');
+const pg = require('pg');
 
-console.log("hola")
+
+const client = new pg({ //connects to the database
+  connectionString: process.env.DATABASE_URL,
+  ssl: {rejectUnauthorized: false}
+});
+client.connect();
+
+client.query(CREATE TABLE bus(
+  VehicleID int,
+  DT datetime,
+  DirectionNum int,
+  Lat decimal,
+  Lon decimal
+))
 function getBus(bus, key){
 	var imp1 = 'https://api.wmata.com/Bus.svc/json/jBusPositions?RouteID='+bus+'&api_key='+key
 	return new Promise(function(resolve, reject){
@@ -11,15 +25,17 @@ function getBus(bus, key){
 			else {resolve(body)};
 })});
 }
-getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79').then(res => {bla = res
-console.log(bla)
-})
-console.log("holo")
 
+(async () => {
+	var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79');
+	console.log(bla);
+})()
 
+/*
 var port = process.env.PORT || 5000; 
 http.createServer(function(req,res){ // creates a server
     res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
     res.end("hello"); 
 
 }).listen(port); // attaches this server to the port no.
+*/
