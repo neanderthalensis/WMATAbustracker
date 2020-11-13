96994b77f3b0ca83c6055683eb93ce1299ca3da0
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-12 20:48:02 -0500

    just work pls

--- server.js
+++ server.js
@@ -13,27 +13,26 @@ function getBus(bus, key){
 }
 
 (async () => {
+
 const client = await new Client({ //connects to the database
   connectionString: process.env.DATABASE_URL,
   ssl: {rejectUnauthorized: false}
 });
+
+var port = process.env.PORT || 5000; 
+http.createServer(function(req,res){ // creates a server
 client.connect();
+
 var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79')
 var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
 var qvals = [new Date(), bla]
 client.query(qimp, qvals)
-
-console.log("asdfhasdjkhf")
-
 })()
 
+console.log("I did it!")
 
 
-/*
-var port = process.env.PORT || 5000; 
-http.createServer(function(req,res){ // creates a server
-    res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
-    res.end("hello"); 
+res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
+res.end("hello"); 
 
 }).listen(port); // attaches this server to the port no.
-*/
