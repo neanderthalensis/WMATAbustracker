5d8692a988291c73473dd336d17c4ce86fe18d7b
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 00:13:08 -0500

    just work pls

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const request = require('request');
 const { Client } = require('pg');
 
 
-var port = process.env.PORT || 5000; 
-http.createServer(function(req,res){ // creates a server
-	res.writeHead(200,{'Content-type':'text/html'}); //Specifies that the respones "hello" is a text
-	fs.readFile('./index.html', null, function (err, data) {
-    if (err) {
-        res.writeHead(404);
-        res.write('The page is nowhere to be found.');
-    } else {
-        res.write(data);
-    }
-    res.end(); 
-}).listen(port); 
+var port = process.env.PORT || 5000;
 
+
+let handleRequest = (req, res) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(port);
