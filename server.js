82bb6f9ae95d18c2555178cdb388d15aff779de9
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 11:23:47 -0500

    just work pls

--- server.js
+++ server.js
@@ -8,18 +8,20 @@ var port = process.env.PORT || 5000;
 
 let handleRequest = (req, res) => {
 
+	if(false){}
+	else{
 	fs.readFile("index.html", function(err, data){
    	if(err){
-      res.writeHead(404);
-      res.write("Not Found!");
-   }
-   else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-   }
-   res.end();
-});
-
+		res.writeHead(404);
+		res.write("Not Found!");
+    }
+    else{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+	  	res.write(data);
+    }
+    res.end();
+    });
+	}
 };
 
 http.createServer(handleRequest).listen(port);
