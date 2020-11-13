6af8b6ea1098d906e83b3b59d4d2828630d2c2e8
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-12 20:50:55 -0500

    just work pls

--- server.js
+++ server.js
@@ -21,18 +21,18 @@ const client = await new Client({ //connects to the database
 
 var port = process.env.PORT || 5000; 
 http.createServer(function(req,res){ // creates a server
-client.connect();
+	client.connect();
 
-var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79')
-var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
-var qvals = [new Date(), bla]
-client.query(qimp, qvals)
+	var bla = await getBus('C4', 'b259cbc5f9a34a0da7192b3679918b79')
+	var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
+	var qvals = [new Date(), bla]
+	client.query(qimp, qvals)
 
-console.log("I did it!")
+	console.log("I did it!")
 
 
-res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
-res.end("hello"); 
+	res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
+	res.end("hello"); 
 
 }).listen(port); // attaches this server to the port no.
 })()
