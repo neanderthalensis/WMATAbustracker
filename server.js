e82b020707f0d538886e43bbbfde114817072ae2
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 11:17:12 -0500

    just work pls

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ var port = process.env.PORT || 5000;
 
 let handleRequest = (req, res) => {
 
-    	res.writeHead(200, {'Content-Type': 'text/html'});
-    	fs.readFile('./index.html', null, function (err, data) {
-        if (err) {
-           	res.writeHead(404);
-           	res.write('The file is nowhere to be found')}
-        else {res.write(data)}  
-    	});
-    	res.end();
+	fs.readFile("index.html", function(err, data){
+   	if(err){
+      response.writeHead(404);
+      response.write("Not Found!");
+   }
+   else{
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+   }
+   response.end();
+});
 
 };
 
