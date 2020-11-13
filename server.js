8a0b222cf1d6d6a667877d0a9e58489a0482b7b6
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 11:07:47 -0500

    just work pls

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const { Client } = require('pg');
 var port = process.env.PORT || 5000;
 
 let handleRequest = (req, res) => {
-	if(req.url == "/index.css"){
+	if(req.url == "/index.css") {
 		res.writeHead(200, {'Content-Type': 'text/css'});
     	fs.readFile('./index.css', null, function (err, data) {
     		if(err){}
 			else{res.write(data)}
-    	})}
+    	})
+    	res.end();
+	    }
     else {
     	res.writeHead(200, {'Content-Type': 'text/html'});
     	fs.readFile('./index.html', null, function (err, data) {
@@ -21,8 +23,8 @@ let handleRequest = (req, res) => {
            	res.write('The file is nowhere to be found')}
         else {res.write(data)}  
     	});
+    	res.end();
 		}
-	res.end();
 };
 
 http.createServer(handleRequest).listen(port);
