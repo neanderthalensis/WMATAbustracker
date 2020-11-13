b20d8538e4c493829f743ba471110deac83e6d96
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 00:17:00 -0500

    just work pls

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const { Client } = require('pg');
 
 var port = process.env.PORT || 5000;
 
-
 let handleRequest = (req, res) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile('./index.html', null, function (err, data) {
         if (err) {
             res.writeHead(404);
@@ -19,5 +18,4 @@ let handleRequest = (req, res) => {
         res.end();
     });
 };
- 
 http.createServer(handleRequest).listen(port);
