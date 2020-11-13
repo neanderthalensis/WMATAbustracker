e062fbd8f7ae631e9b723dac51530801fb05b8b2
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 11:13:04 -0500

    just work pls

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const { Client } = require('pg');
 var port = process.env.PORT || 5000;
 
 let handleRequest = (req, res) => {
-	if(req.url == "/index.css") {
-		res.writeHead(200, {'Content-Type': 'text/css'});
-    	fs.readFile('./index.css', null, function (err, data) {
-    		if(err){}
-			else{res.write(data)}
-    	})
-    	res.end();
-	    }
-    else {
+
     	res.writeHead(200, {'Content-Type': 'text/html'});
     	fs.readFile('./index.html', null, function (err, data) {
         if (err) {
@@ -24,7 +16,7 @@ let handleRequest = (req, res) => {
         else {res.write(data)}  
     	});
     	res.end();
-		}
+
 };
 
 http.createServer(handleRequest).listen(port);
