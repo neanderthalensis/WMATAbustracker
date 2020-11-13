3c33b7038775bba0a12c3a253ef22d62de763a9e
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-13 11:34:50 -0500

    just work pls

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ const { Client } = require('pg');
 
 var port = process.env.PORT || 5000;
 
-let handleRequest = (req, res) => {
-
-	if(false){}
+var handleRequest = (req, res) => {
+	var url = req.url
+	if(url.includes("index.css")){
+		fs.readFile("index.css", function(err, data){
+		if(err){}
+		else{
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+		}
+		})
+	}
 	else{
-	fs.readFile("index.html", function(err, data){
-   	if(err){
-		res.writeHead(404);
-		res.write("Not Found!");
-    }
-    else{
-		res.writeHead(200, {'Content-Type': 'text/html'});
-	  	res.write(data);
-    }
+		fs.readFile("index.html", function(err, data){
+   		if(err){
+			res.writeHead(404);
+			res.write("Not Found!");
+    	}
+    	else{
+			res.writeHead(200, {'Content-Type': 'text/html'});
+		  	res.write(data);
+    	}
     res.end();
     });
 	}
