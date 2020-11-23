6f5a538311b9f86c3cbae81d1d3ef6022cbb822f
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-22 23:57:11 -0500

    styling

--- public/showit.js
+++ public/showit.js
@@ -1,7 +1,13 @@
 
 
-
-
 d3.json('https://wmatabustracker.herokuapp.com/api/bus', function(data) {
     console.log(data);
+    d3.select("body")
+        .selectAll("p")
+        .data(data)
+        .enter()
+        .append("p")
+        .text(function(d) {
+            return d.lat + ", " + d.long;
+        });
 });
