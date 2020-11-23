5b59c9f78af34eb2fc8e6a9f04be1837d7873b08
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-22 23:47:00 -0500

    styling

--- public/showit.js
+++ public/showit.js
@@ -4,11 +4,11 @@ var request = new XMLHttpRequest()
 
 request.open('GET', 'https://wmatabustracker.herokuapp.com/api/bus', true)
 request.onload = function(){
-    var data = JSON.parse(this.response)
-    console.log(data)
-    data.forEach((busloc) => {
-		console.log(busloc.ts)
-	})
+	d3.json{this.response, function(err, data){
+		console.log(data)
+	}
+
+	}
 }
 request.send()
 
