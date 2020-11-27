063de5c7f9869ce3fc0472a55180d63f468375f0
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-27 18:35:48 -0500

    bla

--- updat.js
+++ updat.js
@@ -12,23 +12,21 @@ function getBus(bus, key){
 })});
 }
 
-(async () => {
+var update = async function(){
 	const client = await new Client({ //connects to the database
   		connectionString: process.env.DATABASE_URL,
   		ssl: {rejectUnauthorized: false}
 	});
 
 	client.connect();
-	for(i=0;i<118;i++){
-		var bla = await getBus('C4', process.env.API_KEY)
-		var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
-		var qvals = [new Date(), bla]
-		client.query(qimp, qvals)
-		var qimp2 = "DELETE FROM bus WHERE ts < $1"
-		var qvals2 = [new Date(new Date()-86400000)]
-		client.query(qimp2, qvals2)
-		console.log("I did it!")
-		await new Promise(resolve => setTimeout(resolve, 30000));
-	}
-})()
+	var bla = await getBus('C4', process.env.API_KEY)
+	var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
+	var qvals = [new Date(), bla]
+	client.query(qimp, qvals)
+	var qimp2 = "DELETE FROM bus WHERE ts < $1"
+	var qvals2 = [new Date(new Date()-86400000)]
+	client.query(qimp2, qvals2)
+	console.log("I did it!")
+}
 
+setInterval(update, 30000)
