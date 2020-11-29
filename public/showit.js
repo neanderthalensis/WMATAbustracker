5796fc4657c38016d54ae53b8fb7a019141ce13a
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-28 21:37:25 -0500

    bla

--- public/showit.js
+++ public/showit.js
@@ -1,22 +1,9 @@
 
-function LocLoc(lata, lona, latb, lonb){ // figures out which chunk some position is in
-		if (lata > latb){newver = 1}
-		else if (lata < latb){newver = -1}
-		else {newver = 0};
-
-		if (lona > lonb){newhor = 1}
-		else if (lona < lonb){newhor = -1}
-		else {newhor = 0};
-		return {
-			newhor: newhor,
-			newver: newver
-		}
-
-}
 
 function RoutePos(busdata, dir){ //determines position of the bus and assigns value
 	d3.json('https://wmatabustracker.herokuapp.com/api/route', function(routedata){
-		var filbus = busdata.filter((d) => {return d.direction == dir})
+		console.log(busdata)
+		var filbus = busdata.filter((d) => {return d.directionnum == dir})
 		var filroute;
 		if (dir == 0){filroute = routedata.Direction0.Shape} //bad 
 		else{filroute = routedata.Direction1.Shape}
@@ -38,21 +25,15 @@ function RoutePos(busdata, dir){ //determines position of the bus and assigns va
 		})
 		filbus.forEach((ele, i, arr) => {
 			filroute.forEach((elo, n, aro) => {
-				if (n == 0){
-					var bla = LocLoc(ele.Lat, ele.Lon, elo.Lat, elo.Lon);
-					var ver = bla.newver;
-					var hor = bla.newhor;
-				}
-				else if (n == (aro.length)){}
-				else{
-					var where = LocLoc(ele.Lat, ele.Lon, aro[n+1].Lat, arp[n+1].Lon)
-					if (ver != where.newver && hor != where.newhor){
-						ele.tot = (elo.tot + ((Math.sqrt((Math.abs(elo.Lat - ele.Lat)^2)+(Math.abs(elo.Lon - ele.Lon)^2))/Math.sqrt((Math.abs(elo.Lat - aro[n+1].Lat)^2)+(Math.abs(elo.Lon - aro[n+1].Lon)^2)))*Math.abs(aro[n+1].tot - elo.tot)))/(aro[aro.length].tot)*100 
+				if (n < (aro.length-1)){ //fit it on the route
+				if ((ele.lat >= elo.Lat & ele.lon <= elo.Lon & ele.lat <= aro[n+1].Lat & ele.lon >= aro[n+1].Lon) || (ele.lat <= elo.Lat & ele.lon <= elo.Lon & ele.lat >= aro[n+1].Lat & ele.lon >= aro[n+1].Lon) || (ele.lat <= elo.Lat & ele.lon >= elo.Lon & ele.lat >= aro[n+1].Lat & ele.lon <= aro[n+1].Lon) || (ele.lat >= elo.Lat & ele.lon >= elo.Lon & ele.lat <= aro[n+1].Lat & ele.lon <= aro[n+1].Lon)){
+					if(((elo.Lat-aro[n+1].Lat)/(elo.Lon-aro[n+1].Lon)).toFixed(0) == ((elo.Lat-ele.lat)/(elo.Lon-ele.lon)).toFixed(0)){
+						ele.tot = (elo.tot + (Math.sqrt((Math.abs(elo.Lat - ele.lat)^2)+(Math.abs(elo.Lon - ele.lon)^2))))/(aro[aro.length].tot)
 					}
-					ver = where.newver;
-					hor = where.newhor;
-
 				}
+
+			}
+
 			})
 		})
 
@@ -61,10 +42,8 @@ function RoutePos(busdata, dir){ //determines position of the bus and assigns va
 	});
 };
 
-
+// (Math.sqrt((Math.abs(elo.Lat - ele.lat)^2)+(Math.abs(elo.Lon - ele.lon)^2)))
 d3.json('https://wmatabustracker.herokuapp.com/api/bus', function(data) {
 
-	console.log(data)
-
-    //RoutePos(data, 1)
+    RoutePos(data, 1)
 });
--- updat.js
+++ updat.js
@@ -2,7 +2,7 @@ const http = require('http');
 const request = require('request');
 const { Client } = require('pg');
 
-function getBus(bus, key){ // does the request
+function getBus(bus, key){
 	var imp1 = 'https://api.wmata.com/Bus.svc/json/jBusPositions?RouteID='+bus+'&api_key='+key
 	return new Promise(function(resolve, reject){
 		request.get(//does the request
@@ -12,23 +12,23 @@ function getBus(bus, key){ // does the request
 })});
 }
 
-var update = async function(){
+(async () => {
 	const client = await new Client({ //connects to the database
   		connectionString: process.env.DATABASE_URL,
   		ssl: {rejectUnauthorized: false}
 	});
 
 	client.connect();
-	var bla = await getBus('C4', process.env.API_KEY)
-	var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
-	var qvals = [new Date(), bla]
-	await client.query(qimp, qvals) // inserts new info into the database
-	var qimp2 = "DELETE FROM bus WHERE ts < $1"
-	var qvals2 = [new Date(new Date()-86400000)]
-	await client.query(qimp2, qvals2) // removes old stuff
-	client.end()
-	console.log("I did it!") // spams the logs
+	for(i=0;i<38;i++){
+		var bla = await getBus('C4', process.env.API_KEY)
+		var qimp =  "INSERT INTO bus(ts, json) VALUES($1, $2) RETURNING *"
+		var qvals = [new Date(), bla]
+		client.query(qimp, qvals)
+		var qimp2 = "DELETE FROM bus WHERE ts < $1"
+		var qvals2 = [new Date(new Date()-86400000)]
+		client.query(qimp2, qvals2)
+		console.log("I did it!")
+		await new Promise(resolve => setTimeout(resolve, 30000));
+	}
+})()
 
-}
-
-setInterval(update, 30000) // repeats every 30 seconds...
