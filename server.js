7cfcddee5e370d05c2d46aaf016048059b00575e
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-12-26 19:17:06 -0500

    bla

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.get("/api/routes", (req, res) => { // just passes it along
 });
 
 
-app.get("/api/bus", (req, res) => { // gets stuf from database
-  const { line } = req.params;
+app.get("/api/bus/:r", (req, res) => { // gets stuf from database
+  const r = req.params.r
   client.query("SELECT ts,json_array_elements(json->'BusPositions')->'Deviation' AS Deviation, json_array_elements(json->'BusPositions')->'Lat' AS Lat, json_array_elements(json->'BusPositions')->'Lon' AS Lon, json_array_elements(json->'BusPositions')->'RouteID' AS RouteID, json_array_elements(json->'BusPositions')->'DirectionNum' AS DirectionNum, json_array_elements(json->'BusPositions')->'TripID' AS TripID FROM bus;",
     (error, results) => {
       res.status(200).json(
-        results.rows.filter((d) => {return d.routeid == line})
+        results.rows.filter((d) => {return d.routeid == r})
         );
     }
   );
